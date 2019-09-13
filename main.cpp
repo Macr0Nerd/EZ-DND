ab2d1f79c8e62a17fd50689be9f1a940c42d9024
Macr0Nerd <macronerdart@gmail.com>
2019-09-12 22:03:13 -0500

    Added in a basic server class for receiving files -- Client in development

--- main.cpp
+++ main.cpp
@@ -1,73 +1,25 @@
 #include <iostream>
-#include <utility>
-#include <Poco/StreamCopier.h>
-#include <Poco/FileStream.h>
-#include <Poco/Timespan.h>
-#include <Poco/Thread.h>
-#include <Poco/Runnable.h>
-#include <Poco/Net/SocketAddress.h>
-#include <Poco/Net/Socket.h>
-#include <Poco/Net/StreamSocket.h>
-#include <Poco/Net/ServerSocket.h>
-#include <Poco/Net/SocketStream.h>
+#include <vector>
+#include "Core/network.hpp"
 
 using std::cout;
 using std::cin;
 using std::endl;
 
-class Send : public Poco::Runnable {
-public:
-    Send(std::string  f, int p) : file(std::move(f)), port(p) {};
-
-    virtual void run(){
-        Poco::Net::ServerSocket srvs(Poco::Net::SocketAddress("localhost", port));
-        Poco::Timespan span(250000);
-
-        while(true) {
-            if(srvs.poll(span, Poco::Net::Socket::SELECT_READ)) {
-                Poco::Net::StreamSocket strs = srvs.acceptConnection();
-                Poco::Net::SocketStream ostr(strs);
-
-                Poco::FileInputStream istr(file, std::ios::binary);
-                Poco::StreamCopier::copyStream(istr, ostr);
-            } else {
-                break;
-            }
-        }
-    }
-
-private:
-    int port;
-    std::string file;
-};
-
-class Reci : public Poco::Runnable {
-public:
-    Reci(std::string f, int p) : file(std::move(f)), port(p) {}
-
-    virtual void run() {
-        Poco::Net::StreamSocket ss;
-        ss.connect(Poco::Net::SocketAddress("localhost", port));
-        Poco::Net::SocketStream istr(ss);
-
-        Poco::FileOutputStream ostr(file, std::ios::binary);
-        Poco::StreamCopier::copyStream(istr, ostr);
-    }
-
-private:
-    int port;
-    std::string file;
-};
-
 int main(){
-    Send send("data/fred.ezdc", 1560);
-    Reci reci("test.ezdc", 1560);
-    Poco::Thread threadSend, threadReci;
+    Poco::Net::ServerSocket svs(42069);
 
-    threadSend.start(send);
-    threadReci.start(reci);
-    threadReci.join();
-    threadSend.join();
+    //Configure some server params.
+    auto* pParams = new Poco::Net::TCPServerParams();
+    pParams->setMaxThreads(7);
+    pParams->setMaxQueued(1);
+    pParams->setThreadIdleTime(250000);
+
+    //Create your server
+    Poco::Net::TCPServer myServer(new Poco::Net::TCPServerConnectionFactoryImpl<server>(), svs, pParams);
+    myServer.start();
+
+    while(1);
 
     return 0;
 }
--- src/Core/network.hpp
+++ src/Core/network.hpp
@@ -3,4 +3,147 @@
 #ifndef EZDND_NETWORK_HPP
 #define EZDND_NETWORK_HPP
 
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <array>
+#include <string>
+#include <stdexcept>
+#include <Poco/StreamCopier.h>
+#include <Poco/FileStream.h>
+#include <Poco/Timespan.h>
+#include <Poco/Thread.h>
+#include <Poco/Runnable.h>
+#include <Poco/BinaryWriter.h>
+#include <Poco/Net/SocketAddress.h>
+#include <Poco/Net/Socket.h>
+#include <Poco/Net/StreamSocket.h>
+#include <Poco/Net/ServerSocket.h>
+#include <Poco/Net/SocketStream.h>
+#include <Poco/Net/NetException.h>
+#include <Poco/Net/TCPServer.h>
+
+/**
+ * Because I, Gabriele Ron,
+ * firmly believe in freeware,
+ * open source, and proper attribution,
+ * I need to correctly provide a source for the base of this code
+ *
+ * The base code (for the TCPServer) was built off of
+ * https://stackoverflow.com/questions/16510205/multithread-tcp-server-with-poco-c-libraries
+ * this SO post. This has been modified, but I believe that
+ * I should be honest on the base stuff.
+ *
+ * :)
+ */
+
+class server : public Poco::Net::TCPServerConnection {
+public:
+    explicit server(const Poco::Net::StreamSocket& s) : TCPServerConnection(s) {}
+
+    void sendFile(const std::string& file) {
+        Poco::Net::SocketStream ostr(socket());
+
+        Poco::FileInputStream istr(file, std::ios::binary);
+        Poco::StreamCopier::copyStream(istr, ostr);
+    }
+
+    void recieveFile(const std::string& file) {
+        unsigned char mess[5] = "Ack!";
+        socket().sendBytes(mess, sizeof(mess));
+
+        Poco::Net::SocketStream istr(socket());
+
+        Poco::FileOutputStream ostr(file, std::ios::binary);
+
+        Poco::StreamCopier::copyStream(istr, ostr);
+    }
+
+    void run() final {
+        std::clog << "New connection from: " << socket().peerAddress().host().toString() << std::endl << std::flush;
+        bool isOpen = true;
+        Poco::Timespan timeOut(10, 0);
+        unsigned char inBuff[1000];
+        std::string message;
+
+        while (isOpen) {
+            if (!socket().poll(timeOut, Poco::Net::Socket::SELECT_READ)) {
+                std::cerr << "TIMEOUT!" << std::endl << std::flush;
+            } else {
+                std::clog << "RX EVENT!!! ---> " << std::flush;
+                int nBytes = -1;
+
+                try {
+                    nBytes = socket().receiveBytes(inBuff, sizeof(inBuff));
+                    message = std::string(reinterpret_cast<char const*>(inBuff));
+                    message.erase(message.length()-1);
+                }
+                catch (Poco::Exception &exc) {
+                    //Handle your network errors.
+                    std::cerr << "Network error: " << exc.displayText() << std::endl;
+                    isOpen = false;
+                }
+
+                if (nBytes == 0) {
+                    std::clog << "Client closes connection!" << std::endl << std::flush;
+                    isOpen = false;
+                } else {
+                    std::clog << "Receiving data: " << message << std::endl << std::flush;
+
+                    if(message == "File") {
+                        std::string file;
+                        std::cout << "Enter filename: "; std::cin >> file;
+                        recieveFile(file);
+                    }
+                }
+            }
+        }
+
+        std::cout << "Connection closed" << std::endl << std::flush;
+    }
+};
+
+class client : public Poco::Net::SocketStream {
+public:
+    client(Poco::Net::StreamSocket& ss) : SocketStream(ss){}
+
+    void recieveFile(const std::string& f) {
+        Poco::Net::SocketStream istr(socket());
+        Poco::FileOutputStream ostr(f, std::ios::binary);
+        Poco::StreamCopier::copyStream(istr, ostr);
+    }
+
+    void sendFile(const std::string& f) {
+        unsigned char inBuff[1000];
+        unsigned char mess[5] = "File";
+        std::string message;
+        socket().sendBytes(mess, sizeof(mess));
+
+        int x = 0;
+        while(true) {
+            try {
+                socket().receiveBytes(inBuff, sizeof(inBuff));
+                message = std::string(reinterpret_cast<char const*>(inBuff));
+                message.erase(message.length()-1);
+                if (message == "Ack!") {
+                    break;
+                } else {
+                    continue;
+                }
+            } catch (Poco::Exception& /*e*/){
+                ++x;
+                if(x > 5) {
+                    throw std::out_of_range("No ack received");
+                }
+            }
+        }
+
+        Poco::Net::SocketStream ostr(socket());
+
+        Poco::FileInputStream istr(f, std::ios::binary);
+        Poco::StreamCopier::copyStream(istr, ostr);
+    }
+};
+
 #endif /* EZDND_NETWORK_HPP */
